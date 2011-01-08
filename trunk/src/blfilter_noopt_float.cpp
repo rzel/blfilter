85cc8655a912a6b9eadb0b61930515a27ff2f179
abification <abification@3300584d-b913-8898-4b2d-6e0f65d6bffb>
2011-01-08 19:47:06 +0000

    updated all files

--- trunk/src/blfilter_noopt_float.cpp
+++ trunk/src/blfilter_noopt_float.cpp
@@ -1,242 +1,178 @@
 
 #include "CImg.h"
-#include <omp.h>
-#include <sys/time.h>
 
 using namespace cimg_library;
 CImg<float>  blfilter(CImg<float>, int, float, float);
-double my_difftime();
+
 
 int main(int argc, char* args[]) {
 
 
 //1. input image
-    char* fileName = args[1];
+char* fileName = args[1];
 //2. filter half width
-    int filter_hw = (int)atoi(args[2]);
+int filter_hw = (int)atoi(args[2]);
 //3. sigma_sp (standard deviation for the spatial distance)
-    float sigma_sp = (float)atoi(args[3]);
+float sigma_sp = (float)atoi(args[3]);
 //4. sigma_ph standard deviation for the photometric distance
-    float sigma_ph = (float)atoi(args[4]);
+float sigma_ph = (float)atoi(args[4]);
 
 //load image
-    CImg<float> image(fileName);
+CImg<float> image(fileName);
 
-//load image
+//load image 
 //CImg<float> image("lena.png");
 
-    int dx = image.width();
-    int dy = image.height();
-    int ds = image.spectrum(); //Gray or RGB (or could be more channels);
+int dx = image.width();
+int dy = image.height();
+int ds = image.spectrum(); //Gray or RGB (or could be more channels);
 
 //create a single image dz = 1, rgb channel  = 1 (only gray); fill it with 0s.
-    CImg<float> grayImg(dx,dy,1,1,0);
+CImg<float> grayImg(dx,dy,1,1,0);
 
-    if(ds == 3) { //convert the input RGB to gray image. (work with gray image for now)
-        for(int i = 0; i < dx; i++)
-            for(int j  = 0; j < dy; j++) {
-                float gray_pixel = 0.299 * image(i,j,0,0) +
-                                   0.587 * image(i,j,0,1) +
-                                   0.114 * image(i,j,0,2);
-                //printf("(%d,%d) = (%f) \n", i,j, gray_pixel);
-                grayImg(i,j,0,0) = gray_pixel;
-            }
-    }
-    else
-        grayImg = image;
+if(ds == 3) { //convert the input RGB to gray image. (work with gray image for now)
+	for(int i = 0; i < dx; i++)
+		for(int j  = 0; j < dy; j++) {
+			float gray_pixel = 0.299 * image(i,j,0,0) + 
+				     0.587 * image(i,j,0,1) +
+				     0.114 * image(i,j,0,2);
+			//printf("(%d,%d) = (%f) \n", i,j, gray_pixel);
+			grayImg(i,j,0,0) = gray_pixel;	
+	}
+}
+else
+	grayImg = image;
 
-    printf("spectra = %d\n\n", ds );
-    printf("filtering ... \n\n");
+printf("spectra = %d\n\n", ds );
+printf("filtering ... \n\n");
 
 // this function returns the filtered image
-    clock_t start, end;
-    float elapsed;
+clock_t start, end;
+float elapsed;
 
-    double sttime, endtime;
- 
-    start = clock();
-    sttime = my_difftime();
+start = clock();
 //call the filtering function
-    CImg<float> filteredImg = blfilter(grayImg, filter_hw, sigma_sp, sigma_ph);
-    endtime = my_difftime();
-
-    end = clock();
-    elapsed = ((float) (end - start)) / CLOCKS_PER_SEC;
- 
- // printf("Total CPU cycles = %ld\n", (end - start));
- //   printf("Time taken for filtering = %f \n", elapsed);
-
- // printf("Time take for filtering = %f \n", (endtime - sttime));
+CImg<float> filteredImg = blfilter(grayImg, filter_hw, sigma_sp, sigma_ph);
+end = clock();
+elapsed = ((float) (end - start)) / CLOCKS_PER_SEC;
+printf("Total CPU cycles = %ld\n", (end - start));
+printf("Time taken for filtering = %f \n", elapsed);
 
 //create display windows
-//CImgDisplay main_disp(image, "Lena");
-    CImgDisplay gray_disp(grayImg, "Gray Image");
-    CImgDisplay flt_disp(filteredImg, "Filtered Image");
+//CImgDisplay main_disp(image, "Lena");	
+CImgDisplay gray_disp(grayImg, "Gray Image");
+CImgDisplay flt_disp(filteredImg, "Filtered Image");
 
 //create event loop to display the image
-    while (!gray_disp.is_closed() ) {
-        gray_disp.wait();
-        //flt_disp.wait();
-    }
+while (!gray_disp.is_closed() ) {
+	gray_disp.wait();  
+	//flt_disp.wait();
+        }
 
 }
 
 
 CImg<float>  blfilter(CImg<float> grayImg, int filter_hw, float sigma_sp, float sigma_ph)
 {
-    //image height and width
-    int dx = grayImg.width();
-    int dy = grayImg.height();
+	//image height and width	
+	int dx = grayImg.width();
+	int dy = grayImg.height();
 
+	
+	//create a distance grid(x and y distance from the filter core)
+	//(dont need separate grids as in matlab)
+	//float xgrid[2*filter_hw + 1][2*filter_hw + 1];
+	//float ygrid[2*filter_hw + 1][2*filter_hw + 1]; 
 
-    //create a distance grid(x and y distance from the filter core)
-    //(dont need separate grids as in matlab)
-    //float xgrid[2*filter_hw + 1][2*filter_hw + 1];
-    //float ygrid[2*filter_hw + 1][2*filter_hw + 1];
+	//gaussian kernel for the spatial distances
+	float gaussian_sp[2*filter_hw + 1][2*filter_hw + 1]; 	 
 
-    //gaussian kernel for the spatial distances
-    float gaussian_sp[2*filter_hw + 1][2*filter_hw + 1];
+	for(int i = -filter_hw; i<= filter_hw ; i++)
+		for(int j = -filter_hw; j<= filter_hw; j++) {
+			//xgrid[i+filter_hw][j+filter_hw] = j;
+			//ygrid[i+filter_hw][j+filter_hw] = i;
+			gaussian_sp[i+filter_hw][j+filter_hw] = exp( -(j*j + i*i) / (2*sigma_sp*sigma_sp) );
+			 
+		}
+	 
 
-    for(int i = -filter_hw; i<= filter_hw ; i++)
-        for(int j = -filter_hw; j<= filter_hw; j++) {
-            //xgrid[i+filter_hw][j+filter_hw] = j;
-            //ygrid[i+filter_hw][j+filter_hw] = i;
-            gaussian_sp[i+filter_hw][j+filter_hw] = exp( -(j*j + i*i) / (2*sigma_sp*sigma_sp) );
+	/**
+	//print the kernel for splot (to visualize the kernel using gnuplot)
+	for(int i = -filter_hw; i<= filter_hw ; i++) {
+		for(int j = -filter_hw; j<= filter_hw; j++) {
+			printf(" %d\t%d\t%f \n ", i, j,gaussian_sp[i+filter_hw][j+filter_hw]);
+		}
+	}
+	**/	
 
+	//create empty image for filtered output dz = 1, rgb channel  = 1 (only gray); fill it with 0s.
+	CImg<float> filteredImg(dx,dy,1,1,0);
+	
+	//zero pad the input image to accomodate filtering at the boundary
+	CImg<float> paddedImg(dx + 2 * filter_hw, dy + 2 * filter_hw, 1, 1, 0);
+
+	for(int i = 0; i < dx; i++)
+		for(int j = 0; j < dy; j++)
+			paddedImg(i+filter_hw, j+filter_hw) = grayImg(i,j);	
+
+	/**
+	CImgDisplay pad_disp(paddedImg, "padded image");
+	//create event loop to display the image
+	while (!pad_disp.is_closed()) {
+		pad_disp.wait();  
+		//fil_disp.wait();       
         }
+	**/	 	
 
+	float local_window[2*filter_hw + 1][2*filter_hw + 1]; 	 
+	float local_pixel = 0; //local window
+	float pd = 0; //photometric distance
+	float gaussian_ph = 0; //photometric filter coefficient
+	float gaussian_bl = 0; //bilateral filter coefficient
+	float filtered_pixel = 0; //filtered value of pixel 
+	float normal_factor = 0; //total weight of the filter window used to normalize the filtered coefficient
 
-    /**
-    //print the kernel for splot (to visualize the kernel using gnuplot)
-    for(int i = -filter_hw; i<= filter_hw ; i++) {
-    	for(int j = -filter_hw; j<= filter_hw; j++) {
-    		printf(" %d\t%d\t%f \n ", i, j,gaussian_sp[i+filter_hw][j+filter_hw]);
-    	}
-    }
-    **/
-
-    //create empty image for filtered output dz = 1, rgb channel  = 1 (only gray); fill it with 0s.
-    CImg<float> filteredImg(dx,dy,1,1,0);
-
-    //zero pad the input image to accomodate filtering at the boundary
-    CImg<float> paddedImg(dx + 2 * filter_hw, dy + 2 * filter_hw, 1, 1, 0);
-
-    for(int i = 0; i < dx; i++)
-        for(int j = 0; j < dy; j++)
-            paddedImg(i+filter_hw, j+filter_hw) = grayImg(i,j);
-
-    /**
-    CImgDisplay pad_disp(paddedImg, "padded image");
-    //create event loop to display the image
-    while (!pad_disp.is_closed()) {
-    	pad_disp.wait();
-    	//fil_disp.wait();
-        }
-    **/
-double shared_time = 0;
-#ifdef USE_OMP
-	#pragma omp parallel
-	{
-#endif
-
-   double start = my_difftime();
-
-    int tid, nthreads;
-    int part_idx; 	    
-
-    float local_window[2*filter_hw + 1][2*filter_hw + 1];
-    float local_pixel = 0; //local window
-    float pd = 0; //photometric distance
-    float gaussian_ph = 0; //photometric filter coefficient
-    float gaussian_bl = 0; //bilateral filter coefficient
-    float filtered_pixel = 0; //filtered value of pixel
-    float normal_factor = 0; //total weight of the filter window used to normalize the filtered coefficient
-
-
-	#ifdef USE_OMP
-	nthreads = omp_get_num_threads();
-	//printf("Number of threads = %d\n", nthreads);
-	part_idx = dx / nthreads;
-	
-	 tid = omp_get_thread_num();
-	 //printf("Hello from thread = %d\n", tid);
-	
-	int start_idx = tid*part_idx + filter_hw;
-	int end_idx = (tid + 1)*part_idx + filter_hw;
-
-	if(end_idx > (dx + filter_hw))
-		end_idx = dx + filter_hw;
-	#endif
-		
-	
-	#ifdef USE_OMP	
-	for(int i = start_idx; i < end_idx ; i++)
-	#else
 	for(int i = filter_hw; i < dx + filter_hw ; i++)
-	#endif
-        for(int j = filter_hw; j < dy + filter_hw; j++) {
+		for(int j = filter_hw; j < dy + filter_hw; j++) {
+				
+		filtered_pixel = 0;
+		normal_factor = 0;
 
-            filtered_pixel = 0;
-            normal_factor = 0;
+		//1. calculate the photometric gaussian distances and the filter coefficent (no need to create a full kernel)	
+		//2. multiply each pixel of the window with the spatial and photometric coefficients
 
-            //1. calculate the photometric gaussian distances and the filter coefficent (no need to create a full kernel)
-            //2. multiply each pixel of the window with the spatial and photometric coefficients
+		for(int k = -filter_hw ; k <= filter_hw ; k++)
+			for(int l = -filter_hw ; l <= filter_hw ; l++) {
+			
+			//local_window[k + filtered_hw][l + filtered_hw] = paddedImg(i + k, j + l);			
+			local_pixel = paddedImg(i + k, j + l);
+			
+			//photometric distance = difference in pixel values (squared)
+			pd = paddedImg(i,j) - local_pixel;
+			pd = pd * pd;  
 
-            for(int k = -filter_hw ; k <= filter_hw ; k++)
-                for(int l = -filter_hw ; l <= filter_hw ; l++) {
+			gaussian_ph = exp( -(pd) / (2*sigma_ph*sigma_ph) );
+			gaussian_bl = gaussian_ph * gaussian_sp[k+filter_hw][l+filter_hw] ;
+			
+			filtered_pixel += gaussian_bl * local_pixel;	
+			normal_factor += gaussian_bl;	
+							
+			}
+			
+		filtered_pixel = filtered_pixel / normal_factor;
 
-                    //local_window[k + filtered_hw][l + filtered_hw] = paddedImg(i + k, j + l);
-                    local_pixel = paddedImg(i + k, j + l);
+		//printf("(%d, %d) = %f", i, j, normal_factor);
 
-                    //photometric distance = difference in pixel values (squared)
-                    pd = paddedImg(i,j) - local_pixel;
-                    pd = pd * pd;
-
-                    gaussian_ph = exp( -(pd) / (2*sigma_ph*sigma_ph) );
-                    gaussian_bl = gaussian_ph * gaussian_sp[k+filter_hw][l+filter_hw] ;
-
-                    filtered_pixel += gaussian_bl * local_pixel;
-                    normal_factor += gaussian_bl;
-
-                }
-
-            filtered_pixel = filtered_pixel / normal_factor;
-
-            //printf("(%d, %d) = %f", i, j, normal_factor);
-
-            filteredImg(i-filter_hw, j - filter_hw) =  filtered_pixel;
+		filteredImg(i-filter_hw, j - filter_hw) =  filtered_pixel;
 
 
-        }
-
-double end = my_difftime();
-if( (end - start) > shared_time)
-	shared_time = end - start;
-     
-#ifdef USE_OMP
-if(tid == 0) 
-printf("total time for filtering = %f\n", shared_time);
-
-	}	//pragma omp parallel		
-#else
-printf("total time for filtering = %f\n", shared_time);
-
-#endif		
-
-    return filteredImg;
+		}			
+				
+	return filteredImg;
 
 }
 
 
-double my_difftime ()
-{
-    struct timeval tp;
-    struct timezone tzp;
-    int i;
-
-    i = gettimeofday(&tp,&tzp);
-    return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
-};
 
 
 
