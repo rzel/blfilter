ed6e0389739bb3e9b8d2471c8bf6d0fbcc46dbb1
dinwal <dinwal@3300584d-b913-8898-4b2d-6e0f65d6bffb>
2011-02-11 00:22:31 +0000

    Updated run script

--- run.sh
+++ run.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
-echo bfilter
-#nice -19 bin/bfilter $1 $2 $3 $4 
-echo bfilterOMP
-#nice -19 bin/bfilterOMP $1 $2 $3 $4 
-echo bfilterTile
-nice -19 bin/bfilterTile $1 $2 $3 $4 $5 
-echo bfilterTileOMP
-nice -19 bin/bfilterTileOMP $1 $2 $3 $4 $5 
-echo bfilterExpTile
-nice -19 bin/bfilterExpTile $1 $2 $3 $4 $5 
-echo bfilterExpTileOMP
-bin/bfilterExpTileOMP $1 $2 $3 $4 $5 
+echo bin/bNptD $1 $2 $3 $4 $5 
+bin/bNptD $1 $2 $3 $4 $5 
+echo bin/bNptF $1 $2 $3 $4 $5 
+bin/bNptF $1 $2 $3 $4 $5 
+echo bin/bNptFOMP $1 $2 $3 $4 $5 
+bin/bNptFOMP $1 $2 $3 $4 $5 
+echo bin/bSSEa $1 $2 $3 $4 $5 
+bin/bSSEa $1 $2 $3 $4 $5 
+echo bin/bSSEaR $1 $2 $3 $4 $5 
+bin/bSSEaR $1 $2 $3 $4 $5 
+echo bin/bSSEI $1 $2 $3 $4 $5 
+bin/bSSEI $1 $2 $3 $4 $5 
+echo bin/bSSEIR $1 $2 $3 $4 $5 
+bin/bSSEIR $1 $2 $3 $4 $5 
+echo bin/bLlw $1 $2 $3 $4 $5 
+bin/bLlw $1 $2 $3 $4 $5 
+echo bin/bL2w $1 $2 $3 $4 $5 
+bin/bL2w $1 $2 $3 $4 $5 
+#bin/bfilter $1 $2 $3 $4 $5 
+#bin/bfilter $1 $2 $3 $4 $5 
+#bin/bfilter $1 $2 $3 $4 $5 
+#bin/bfilter $1 $2 $3 $4 $5 
+#bin/bfilter $1 $2 $3 $4 $5 
+#echo bfilter
+#bin/bfilter $1 $2 $3 $4 $5 
+#echo bfilterOMP
+#bin/bfilterOMP $1 $2 $3 $4 $5
+#echo bfilterTile
+#bin/bfilterTile $1 $2 $3 $4 $5 $6  
+#echo bfilterTileOMP
+#bin/bfilterTileOMP $1 $2 $3 $4 $5 $6  
+#echo bfilterExpTile
+#bin/bfilterExpTile $1 $2 $3 $4 $5 $6
+#echo bfilterExpTileOMP
+#bin/bfilterExpTileOMP $1 $2 $3 $4 $5 $6 
 
