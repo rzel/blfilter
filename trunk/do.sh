45a66517fae56aa83d7fe7af3e673b4c2c46188a
dinwal <dinwal@3300584d-b913-8898-4b2d-6e0f65d6bffb>
2011-02-26 20:48:08 +0000

    Scripts for openmp comparison

--- trunk/do.sh
+++ trunk/do.sh
@@ -1,3 +1,10 @@
-sh exe.sh images/lena.png 10 10 10 0 64
-sh exe.sh images/niagarafalls_aerial_photo.jpg 10 10 10 0 100
-
+#sh exe.sh images/lena.png 10 10 10 0 64
+#sh exe.sh images/niagarafalls_aerial_photo.jpg 10 10 10 0 100
+p=1;
+p=$1;
+for (( i=1; i<=p; i++ ))
+do
+    export OMP_NUM_THREADS=$i
+    #sh exe.sh images/lena.png 10 10 10 0 64
+    sh exe.sh images/niagarafalls_aerial_photo.jpg 10 10 10 0 100
+done
--- trunk/exe.sh
+++ trunk/exe.sh
@@ -2,17 +2,6 @@
 
 for (( i=1; i<=10; i++ ))
 do
-    bin/bSSEIMI $1 $2 $3 $4 $5 $6 
-    echo 
-done
-
-for (( i=1; i<=10; i++ ))
-do
-    bin/bSSEI $1 $2 $3 $4 $5 $6 
-    echo 
-done
-for (( i=1; i<=10; i++ ))
-do
     bin/bSSEIOMP $1 $2 $3 $4 $5 $6 
     echo 
 done
